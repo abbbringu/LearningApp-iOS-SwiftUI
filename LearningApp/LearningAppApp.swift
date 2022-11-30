63226d43d40b21a45efc6c4dfec63af30c1fe192
abbbringu <brianphi.nguyen@gmail.com>
2022-11-30 21:18:10 +0100

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Brian Nguyen on 2022-11-30.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
