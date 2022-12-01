4f412edecf92b368f221dc107453d1213db71a59
abbbringu <brianphi.nguyen@gmail.com>
2022-12-01 18:35:29 +0100

    - add assets and data file - parse json

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -8,5 +8,39 @@
 import Foundation
 
 class ContentModel: ObservableObject {
+    @Published var modules = [Module]()
+    var styleData: Data?
+    init() {
+        getLocalData()
+    }
     
+    func getLocalData() {
+        // Parse the json data
+        // Get the url of the file in the bundle
+        let jsonURL = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            // Access the data with the URL
+            let jsonData = try Data(contentsOf: jsonURL!)
+            // Init the decoder
+            let decoder = JSONDecoder()
+            // State how we want to decode, and from where
+            let modules = try decoder.decode([Module].self, from: jsonData)
+            // Setting the modules in the top scope of the class
+            self.modules = modules
+        } catch {
+            print(error)
+        }
+        
+        
+        let styleURL = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            let styleData = try Data(contentsOf: styleURL!)
+            self.styleData = styleData
+        } catch {
+            print(error)
+        }
+       
+    }
 }
